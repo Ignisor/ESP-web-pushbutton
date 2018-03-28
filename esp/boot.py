5cfd46f2fc05de2f0b110273b31d3b7925086fcd
ignisor <ignis2497@gmail.com>
2018-03-28 12:36:06 +0300

    using statics for wifi - for faster connection

--- esp/boot.py
+++ esp/boot.py
@@ -12,18 +12,21 @@ URL = 'http://door.gowombat.team/open/'
 LED = machine.Pin(1, machine.Pin.OUT)
 
 
-LED.value(0)  # enable LED
+# enable LED
+LED.value(0)
 
-ap_if = network.WLAN(network.AP_IF)
 sta_if = network.WLAN(network.STA_IF)
+if not sta_if.active():
+    sta_if.active(True)
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
 
-ap_if.active(False)
-sta_if.active(True)
+# connect to wifi (most settings specified as statics for speedup)
+sta_if.ifconfig(('192.168.1.82', '255.255.255.0', '192.168.1.1', '192.168.1.1'))
+sta_if.config(mac=b'\x18\xfe4\xd5i\x1c', dhcp_hostname='ESP_D5691C')
+sta_if.connect("wombat_3", "fantomas", bssid=b'\xd8P\xe6\xd9$\x10')
 
-# connect to wifi
 t_start = time.time()
-sta_if.connect("wombat_3", "fantomas")
-
 while not sta_if.isconnected():
     t = time.time() - t_start
     if t >= TIMEOUT:
